f751230eb144d33c122974d14d1f28d20a2928dd
John Trammell <johntrammell@gmail.com>
2011-01-10 15:30:15 -0600

    implementing commandline options

--- ch01/wallpaper.py
+++ ch01/wallpaper.py
@@ -1,18 +1,44 @@
 #!/usr/bin/python2.5
 
-import PIL
+import getopt
+import sys
 from PIL import Image, ImageColor
 
-im = Image.new('RGB',(200,200))
-white = ImageColor.getrgb('#fff')
-for x in range(90,110):
-    for y in range(90,110):
-        im.putpixel((x,y),white)
-im.save('wallpaper.png')
+def usage():
+    print """\
+Usage:
 
-# for i = 1..100:
-#     for j = 1..100:
-#         x = corner_a + i * side / 100
-#         y = corner_b + j * side / 100
-#         c = int(x^2 + y^2)
+  wallpaper.py --scale=<scale>
 
+# FIXME: need to add command-line options:
+#   - corner_a, corner_b, size, color
+
+
+    """
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "hg:d", ["help", "grammar="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
+    side = 234
+    corner_a = 10
+    corner_b = 10
+
+    im = Image.new('RGB', (100, 100))   # init to black
+    white = ImageColor.getrgb('#fff')
+
+    for i in range(0, 100):
+        for j in range(0, 100):
+            x = corner_a + float(side) * float(i) / 100.0
+            y = corner_b + float(side) * float(j) / 100.0
+            c = int(x * x + y * y) % 2
+            if c == 0:
+                im.putpixel((i, j), white)
+
+    im.save('wallpaper.png')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
