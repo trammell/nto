090fe67158fad208953b4d399e03dc3aad076702
John Trammell <johntrammell@gmail.com>
2011-01-09 22:29:48 -0600

    starting to add python

--- ch01/wallpaper.py
+++ ch01/wallpaper.py
@@ -1,5 +1,9 @@
-#!/usr/bin/ruby
+#!/usr/bin/python
 
-
+for i = 1..100:
+    for j = 1..100:
+        x = corner_a + i * side / 100
+        y = corner_b + j * side / 100
+        c = int(x^2 + y^2)
 
 
